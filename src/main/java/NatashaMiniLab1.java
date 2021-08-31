ee66f5920a635974b1fad7df5c6818718300ad0a
mistylavender <89210459+mistylavender@users.noreply.github.com>
2021-08-31 09:45:49 -0700

    navbar changed, minilab

---
+++ src/main/java/NatashaMiniLab1.java
@@ -0,0 +1,20 @@
+public class NatashaMiniLab1 {
+    public static void main(String[] args){
+        String name = "Natasha Kapoor";
+
+        double avgGrade;
+        avgGrade = 97.90;
+
+        final int numClass;
+        numClass = 5;
+
+        boolean atDelNorte;
+        atDelNorte = true;
+
+        System.out.print(name + "has an average grade of " + avgGrade);
+
+        System.out.println(name + "is taking " + numClass + " classes.");
+
+        System.out.println(name + "is currently at Del Norte for the 2021-22 school year: " + atDelNorte);
+    }
+}
