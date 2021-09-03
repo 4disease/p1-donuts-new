09f6882add52fad6087768c694051aa527645902
sam <70492417+dsblack0@users.noreply.github.com>
2021-09-02 17:03:37 -0700

    added minilab (in progress)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Task Manager
-Welcome to our project! 
+Welcome to our project!
 
 Kira and Natasha [journal](https://docs.google.com/document/d/1QwZR7rZCKQ_408GuY0SUCDS9y_yJ-nyMV44ZrVq_2t0/edit?usp=sharing) <br/>
-Adhithi, Sam, and Ridhima [journal](https://padlet.com/ridhimai32042/hjlr1j1n8tl5978i)
+Adhithi, Sam, and Ridhima [journal](https://docs.google.com/document/d/16K6aheHJTJdytH5vH_9Q-GDjV7uvpKgQ-4cHytyWjlo/edit?usp=sharing)
 
 Our [scrum board](https://github.com/4disease/p1-donuts/projects)
---
+++ src/main/java/com/example/sping_portfolio/minilabs/Sam.java
@@ -0,0 +1,23 @@
+package com.example.sping_portfolio.minilabs;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Sam {
+    @GetMapping("/sam")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Human") String name,
+                           @RequestParam(name="animal", required=false, defaultValue="Animal") String animal,
+                           @RequestParam(name="number1", required=false, defaultValue="3") String number1,
+                           @RequestParam(name="number2", required=false, defaultValue="2") String number2, Model model){
+        model.addAttribute("name", name);
+        model.addAttribute("animal", animal);
+        model.addAttribute("number1", number1);
+        model.addAttribute("number2", number2);
+        return "sam";
+
+    }
+
+}
