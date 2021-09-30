38f9c2d2cd7c27e119f5272c7107d3cfaec4022b
adhithin <adhithi.nmurthy07@gmail.com>
2021-09-29 21:21:28 -0700

    says fibonacci lab working + image stuff has begun

---
+++ src/main/java/com/example/sping_portfolio/minilabs/Getter.java
@@ -0,0 +1,5 @@
+package com.example.sping_portfolio.minilabs;
+
+public @interface Getter {
+
+}
--- src/main/java/com/example/sping_portfolio/minilabs/fib.java
+++ src/main/java/com/example/sping_portfolio/minilabs/fib.java
@@ -1,26 +1,39 @@
 package com.example.sping_portfolio.minilabs;
 import java.util.Scanner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.String;
+
+
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 
 public class fib {
 
-    public static void main(String[] args) {
-		
-		//this is using Scanner, an import from Java. it reads the input we type into the class. 
-		Scanner input = new Scanner(System.in); // remember the System.out.print() function? this is the opposite of that 
+    @GetMapping("/fib")
+    public String greeting(@RequestParam(name="start", required=false, defaultValue="1") String start, Model model){
+    model.addAttribute("start", start);
+     
 
-		System.out.println("Welcome to Fibonacci. How many numbers of the Fibonacci sequence do you need?");
+        int fnum = Integer.parseInt(start);
+        int count = 0;
 
-		String fib = input.nextLine(); // our string, fib, is the input that the user puts in on the next line. 
+        List<String> fibList = new ArrayList<String>();
 
-		int fnum = Integer.parseInt(fib); //Integer.parseInt() is a function that turns a String into an integer. 
+        int num1 = 0;
+        int num2 = 1;
 
-		int num1 = 0, num2 = 1;
+        fibList.add("0");
+        fibList.add("1");
 
-		int count = 0;
+        while (count < (fnum - 2)) {
   
-        // Iterate till counter is less than our number that was entered in
-        while (count < fnum) {
-  
+            
+            
             // Print the number
             System.out.print(num1 + " ");
   
@@ -28,19 +41,90 @@ public class fib {
             int num3 = num2 + num1;
             num1 = num2;
             num2 = num3;
-            count = count + 1;
+            String numstr3 = String.valueOf(num3); // changes integer to string 
+            count += 1;
+
+            fibList.add(numstr3);
+
+
 
         }
 
-		int next = count + 1; 
 
-		System.out.print("It took " + count + " iterations of the while loop.");
-		System.out.println("the " + next + "th number of the fibonacci sequence is " + num1);
+        model.addAttribute("fibList", fibList);
 
-		//Your goal: Using the for iteration, create a list that will list all 
+        return "/minilabs/fib";
+    }
+
+    public static void wait(int ms)
+    {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+		
+		//this is using Scanner, an import from Java. it reads the input we type into the class. 
+		Scanner scan = new Scanner(System.in); // remember the System.out.print() function? this is the opposite of that
+
+		System.out.println("Welcome to Geometric. Make your purchase here. What would be your starting number?");
+		int start = scan.nextInt();
+        scan.close();
+
+
+        
+        wait(5000);
+        System.out.println(".");
+        wait(5000);
+        System.out.println(".");
+        wait(5000);
+        System.out.println(". \n");
+
+        System.out.println("Here is the geometric sequence you ordered: ");
+        int fnum = start;
+        int count = 0;
+        List<String> fibList = new ArrayList<String>();
+
+        int num1 = 0;
+        int num2 = 1;
+
+        fibList.add("0");
+        fibList.add("1");
+        
+
+        while (count < (fnum - 2)) {
+
+
+            
+            // Print the number
+            System.out.print(num1 + " ");
+  
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            String numstr3 = String.valueOf(num3); // changes integer to string 
+            count += 1;
+
+            fibList.add(numstr3);
+
+
+        }
+
+        System.out.println(fibList);
+
+
+        System.out.println("Thank you for shopping at Geometric. Have a good day!");
 
 		
     }
 
     
 }
+
